b1e41a80d1ecc8cc146e31a5abbb0b39c29f11d9
11philip22 <philipwoldhek@gmail.com>
2019-11-20 20:06:22 +0100

    cown

--- root/usr/local/bin/install.sh
+++ root/usr/local/bin/install.sh
@@ -7,8 +7,12 @@ echo -e "\e[32m[INFO] Unpacking s6 overlay \e[0m"
 gunzip -c /tmp/s6-overlay-amd64.tar.gz | tar -xf - -C /
 rm -rf /tmp/*
 
+echo -e "\e[32m[INFO] Creating user \e[0m"
+useradd -u 1000 -U -d /home/user -m -s /sbin/nologin user
+
 echo -e "\e[32m[INFO] Creating folders \e[0m"
 mkdir -p /config /defaults
 
-echo -e "\e[32m[INFO] Creating user \e[0m"
-useradd -u 1000 -U -d /home/user -m -s /sbin/nologin user
+echo -e "\e[32m[INFO] Fixing permissions \e[0m"
+chown -R user:user /config
+chown -R user:user /defaults
