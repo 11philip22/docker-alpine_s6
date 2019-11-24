3d6ee9bca0a337a9250ec2eccddc68e7982d620f
11philip22 <philipwoldhek@gmail.com>
2019-11-24 17:40:00 +0100

    no home

--- root/usr/local/bin/install.sh
+++ root/usr/local/bin/install.sh
@@ -8,7 +8,8 @@ gunzip -c /tmp/s6-overlay-amd64.tar.gz | tar -xf - -C /
 rm -rf /tmp/*
 
 echo -e "\e[32m[INFO] Creating user \e[0m"
-useradd -u 1000 -U -d /home/user -m -s /sbin/nologin user
+# useradd -u 1000 -U -d /home/user -m -s /sbin/nologin user
+useradd -u 1000 -U -s /sbin/nologin user
 
 echo -e "\e[32m[INFO] Creating folders \e[0m"
 mkdir -p /config /defaults
